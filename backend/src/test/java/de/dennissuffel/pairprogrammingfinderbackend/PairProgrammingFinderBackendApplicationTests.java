b0762956305562c3d5ff88726e8a9e012ba0dda5
Dennis Suffel <80106155+DennisSuffel@users.noreply.github.com>
2021-03-07 15:54:21 +0100

    Add initial structure of backend
    Added a basic project structure for a Spring Boot project generated by Spring Initializr

---
+++ backend/src/test/java/de/dennissuffel/pairprogrammingfinderbackend/PairProgrammingFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dennissuffel.pairprogrammingfinderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairProgrammingFinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
